81ba146af05c41d2bc1c5386322fff805af0086e
wayhi <wayhi.wang@gmail.com>
2016-07-21 16:45:40 +0800

    0721-2

--- resources/views/orders/show_fields.blade.php
+++ resources/views/orders/show_fields.blade.php
@@ -181,20 +181,23 @@
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
     @ability('order_approver', 'admin,owner')
-    @if($order->process_status==1)
-    {!! Form::submit('平台审核通过', ['class' => 'btn btn-success','name'=>'action']) !!}
-    {!! Form::submit('拒绝', ['class' => 'btn btn-danger','name'=>'action']) !!}
-    @endif
-    @if($order->process_status==2)
-    {!! Form::submit('已放款', ['class' => 'btn btn-success','name'=>'action']) !!}
-    @endif
-    @if($order->process_status==4)
-    {!! Form::submit('还款完成', ['class' => 'btn btn-success','name'=>'action']) !!}
-    
-    @endif
-    @if($order->process_status<=4 && $order->process_status>0)
-    {!! Form::submit('订单取消', ['class' => 'btn btn-warning','name'=>'action']) !!}
-    @endif
+        @if($order->process_status==1)
+        {!! Form::submit('平台审核通过', ['class' => 'btn btn-success','name'=>'action']) !!}
+        {!! Form::submit('拒绝', ['class' => 'btn btn-danger','name'=>'action']) !!}
+        @endif
+    @endability
+    @ability('finance_approver', 'admin,owner')
+        @if($order->process_status==2)
+        {!! Form::submit('已放款', ['class' => 'btn btn-success','name'=>'action']) !!}
+        @endif    
+        @if($order->process_status==4)
+        {!! Form::submit('还款完成', ['class' => 'btn btn-success','name'=>'action']) !!}
+        @endif
+    @endability 
+    @ability('order_approver,finance_approver', 'admin,owner')   
+        @if($order->process_status<=4 && $order->process_status>0)
+        {!! Form::submit('订单取消', ['class' => 'btn btn-warning','name'=>'action']) !!}
+        @endif
     @endability
     <a href="javascript:history.go(-1);" class="btn btn-default">返回</a> 
 </div>
